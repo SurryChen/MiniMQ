0d21f5c01c5daee50b53983d073610622b876c27
SurryChen <1974023122@qq.com>
2023-10-11 00:14:35 +0800

    [feat] 定义网络服务的生命周期

--- network/src/main/java/com/somecode/minimq/network/NetworkService.java
+++ network/src/main/java/com/somecode/minimq/network/NetworkService.java
@@ -1,4 +1,18 @@
 package com.somecode.minimq.network;
 
+/**
+ * 定义一个网络服务的生命周期
+ */
 public interface NetworkService {
+
+    /**
+     * 启动服务
+     */
+    void start();
+
+    /**
+     * 销毁服务
+     */
+    void shutdown();
+
 }
